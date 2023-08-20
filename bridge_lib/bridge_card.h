7c1b0cc3e5c8d85e694ac2575fb975669a68fc9a
hanyuu1 <13738325103@126.com>
2023-08-20 15:52:19 +0800

    Init

---
+++ bridge_lib/bridge_card.h
@@ -0,0 +1,7 @@
+#ifndef BRIDGE_CARD
+#define BRIDGE_CARD
+namespace bridge{
+    
+}
+
+#endif /* BRIDGE_CARD */
---
+++ bridge_lib/bridge_game.h
@@ -0,0 +1,7 @@
+#ifndef BRIDGE_GAME
+#define BRIDGE_GAME
+namespace bridge{
+    
+}
+
+#endif /* BRIDGE_GAME */
