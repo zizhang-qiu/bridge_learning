d09286b5e2e27c21ff5cf9a7ff114233170a19f7
hanyuu1 <13738325103@126.com>
2023-10-29 15:30:46 +0800

    update

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/bridge
-# Build directory: D:/Projects/bridge/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pybind11")
