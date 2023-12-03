92cce46ac8b532e1a005541c3d9bb762ccc967d9
hanyuu1 <13738325103@126.com>
2023-12-03 19:48:32 +0800

    Update.

--- bridge_lib/utils.h
+++ bridge_lib/utils.h
@@ -1,13 +1,13 @@
 #ifndef BRIDGE_LEARNING_BRIDGE_LIB_UTILS_H
 #define BRIDGE_LEARNING_BRIDGE_LIB_UTILS_H
-#include <memory>
 #include <cassert>
+#include <iostream>
+#include <memory>
 #include <random>
+#include <sstream>
 #include <stdexcept>
 #include <string>
-#include <iostream>
 #include <unordered_map>
-#include <sstream>
 
 namespace bridge_learning_env {
 using GameParameters = std::unordered_map<std::string, std::string>;
@@ -15,31 +15,20 @@ using GameParameters = std::unordered_map<std::string, std::string>;
 // Returns string associated with key in params, parsed as template type.
 // If key is not in params, returns the provided default value.
 template<class T>
-T ParameterValue(const GameParameters&params,
-                 const std::string&key,
-                 T default_value);
+T ParameterValue(const GameParameters& params, const std::string& key, T default_value);
 
 template<>
-int ParameterValue(const GameParameters&params,
-                   const std::string&key,
-                   int default_value);
+int ParameterValue(const GameParameters& params, const std::string& key, int default_value);
 template<>
-double ParameterValue(const GameParameters&params,
-                      const std::string&key,
-                      double default_value);
+double ParameterValue(const GameParameters& params, const std::string& key, double default_value);
 template<>
-std::string ParameterValue(const GameParameters&params,
-                           const std::string&key,
-                           std::string default_value);
+std::string ParameterValue(const GameParameters& params, const std::string& key, std::string default_value);
 template<>
-bool ParameterValue(const GameParameters&params,
-                    const std::string&key,
-                    bool default_value);
+bool ParameterValue(const GameParameters& params, const std::string& key, bool default_value);
 
 template<typename... Args>
-std::string StrFormat(const std::string&format, Args... args) {
-  int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) +
-      1; // Extra space for '\0'
+std::string StrFormat(const std::string& format, Args... args) {
+  int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
   if (size_s <= 0) {
     throw std::runtime_error("Error during formatting.");
   }
@@ -53,10 +42,10 @@ std::vector<int> Arange(int start, int end);
 
 std::vector<int> Permutation(int num);
 
-std::vector<int> Permutation(int num, std::mt19937&rng);
+std::vector<int> Permutation(int num, std::mt19937& rng);
 
 template<typename T>
-void PrintVector(const std::vector<T>&vec) {
+void PrintVector(const std::vector<T>& vec) {
   for (const T item : vec) {
     std::cout << item << ",";
   }
@@ -72,24 +61,28 @@ std::string StrCat(const Args&... args) {
 }
 
 #if defined(NDEBUG)
-#define REQUIRE(expr)                                                        \
-  (expr ? (void)0                                                            \
-        : (fprintf(stderr, "Input requirements failed at %s:%d in %s: %s\n", \
-                   __FILE__, __LINE__, __func__, #expr),                     \
+#define REQUIRE(expr)                                                                                              \
+  (expr ? (void)0                                                                                                  \
+        : (fprintf(stderr, "Input requirements failed at %s:%d in %s: %s\n", __FILE__, __LINE__, __func__, #expr), \
            std::abort()))
 #else
 #define REQUIRE(expr) assert(expr)
 #endif
 
 #if defined(NDEBUG)
-#define REQUIRE_EQ(expr1, expr2)                                                        \
-(expr1==expr2 ? (void)0                                                            \
-: (fprintf(stderr, "Input requirements failed at %s:%d in %s: %s vs %s\n", \
-__FILE__, __LINE__, __func__, #expr1, #expr2),                     \
-std::abort()))
+#define REQUIRE_EQ(expr1, expr2)                                                     \
+  (expr1 == expr2 ? (void)0                                                          \
+                  : (fprintf(stderr,                                                 \
+                             "Input requirements failed at %s:%d in %s: %s vs %s\n", \
+                             __FILE__,                                               \
+                             __LINE__,                                               \
+                             __func__,                                               \
+                             #expr1,                                                 \
+                             #expr2),                                                \
+                     std::abort()))
 #else
 #define REQUIRE(expr) assert(expr)
 #endif
-} // namespace bridge
+} // namespace bridge_learning_env
 
 #endif // BRIDGE_LEARNING_BRIDGE_LIB_UTILS_H
