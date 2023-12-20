5a5eb624ac3b22d4c0b934eb2a649602fab04ad5
hanyuu1 <13738325103@126.com>
2023-12-20 12:40:53 +0800

    Update spdlog.

--- test.cc
+++ test.cc
@@ -2,30 +2,54 @@
 // Created by qzz on 2023/12/16.
 //
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
-template <typename T, typename Predicate>
-void MoveItemToFirst(std::vector<T>& vec, Predicate constraint_function) {
-  auto it = std::find_if(vec.begin(), vec.end(), constraint_function);
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h"
 
-  if (it != vec.end()) {
-    // Rotate the vector so that the item satisfying the constraint is at the beginning
-    std::rotate(vec.begin(), it, it + 1);
+void rotating_example() {
+  // Create a file rotating logger with 5 MB size max and 3 rotated files
+  auto max_size = 1048576 * 5;
+  auto max_files = 3;
+  auto logger = spdlog::rotating_logger_mt("some_logger_name", "logs/rotating.txt", max_size, max_files);
+}
+void basic_logfile_example() {
+  try {
+    auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
+    logger->error("This is an error.");
+  }
+  catch (const spdlog::spdlog_ex &ex) {
+    std::cout << "Log init failed: " << ex.what() << std::endl;
   }
 }
-
-// Example usage:
+void stdout_example() {
+  // create a color multi-threaded logger
+  auto console = spdlog::stdout_color_mt("console");
+  auto err_logger = spdlog::stderr_color_mt("stderr");
+  spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
+}
 int main() {
-  std::vector<int> yourVector = {2, 8, 4, 6, 1, 5};
+  spdlog::info("Welcome to spdlog!");
+  spdlog::error("Some error message with arg: {}", 1);
 
-  // Using a lambda function as the constraint
-  MoveItemToFirst(yourVector, [](int x) { return x ==6; });  // Move even numbers to the first position
+  spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+  spdlog::info("Support for floats {:03.2f}", 1.23456);
+  spdlog::info("Positional args are {1} {0}..", "too", "supported");
+  spdlog::info("{:<30}", "left aligned");
 
-  // Print the vector to verify the result
-  for (const auto& item : yourVector) {
-    std::cout << item << " ";
-  }
+  spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+  spdlog::debug("This message should be displayed..");
 
-  return 0;
+  // change log pattern
+  spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+  // Compile time log levels
+  // define SPDLOG_ACTIVE_LEVEL to desired level
+  SPDLOG_TRACE("Some trace message with param {}", 42);
+  SPDLOG_DEBUG("Some debug message");
+
+  stdout_example();
+  basic_logfile_example();
 }
